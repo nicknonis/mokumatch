cc8273d6f399f3239b88d01d50a3485b150941ec
Nick <nicknonis@users.noreply.github.com>
2017-07-01 23:33:42 +0800

    overhaul #1
    -swap functions to allow more modularity.
    -reduced hardcoding.
    -user group(region) manipulation
    -change naming convention to bot
    
    #todo
    -fix help function (currently it does noting..)

--- moku.py
+++ moku.py
@@ -1,7 +1,7 @@
-#for now this moku does nothing and is infact just the example bot.
+#for now this bot does nothing and is infact just the example bot.
 import discord
 import asyncio
-import random 
+import random #can remove just using for randomizer game
 import logging
 from discord.ext import commands
 
@@ -17,28 +17,37 @@ tokenFile = open("token", 'r')
 token = tokenFile.read()
 tokenFile.close()
 
-moku = discord.Client()
+
 
 '''
 -----------------------
         BOT JOIN
 -----------------------
 '''
-@moku.event
+sudo_list = ['329513316835655680','329513060614012928'] #mod and admin user groups
+region_list = ['NA', 'EU', 'AUS', 'ASIA','SEA']
+
+prefix = '~' #can swap to array to allow more prefixes...
+description = "Moku the lobby and matchmaking bot"
+bot = commands.Bot(command_prefix=prefix, description=description, pm_help=None)
+
+
+@bot.event
 async def on_ready():
     # Display Login Status in Console
     print('<---------------------------->')
     print('Logged in as')
-    print(moku.user.name)
-    print(moku.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('<---------------------------->')
     while True:
-        await moku.change_presence(game=discord.Game(name='Combot is my friend.'))
+        await bot.change_presence(game=discord.Game(name='Combot is my friend.'))
         await asyncio.sleep(15)
-        await moku.change_presence(game=discord.Game(name='type ~help for assistance!'))
+        await bot.change_presence(game=discord.Game(name='type ~help for assistance!'))
         await asyncio.sleep(10000)
-        await moku.change_presence(game=discord.Game(name='git gud'))
+        await bot.change_presence(game=discord.Game(name='git gud'))
         await asyncio.sleep(30)
+
 
 '''
 --------------------
@@ -46,72 +55,164 @@ async def on_ready():
 --------------------
 '''        
 
-@moku.event        
+@bot.event        
 async def on_member_join(member):
     server = member.server
-    moku_fmt = 'Welcome {0.mention} to {1.name}!'
-    await moku.send_message(server, moku_fmt.format(member, server))
-        
+    bot_fmt = 'Welcome {0.mention} to {1.name}!'
+    await bot.send_message(server, bot_fmt.format(member, server))
+
+
+
+'''
+    ---------------------
+            HELP Func
+    ---------------------
+'''    
+@asyncio.coroutine
+def _default_help_command(ctx, *commands : str):
+    """Shows this message."""
+    bot = ctx.bot
+    destination = ctx.message.author if bot.pm_help else ctx.message.channel
+
+    def repl(obj):
+        return _mentions_transforms.get(obj.group(0), '')
+
+    # help by itself just lists commands.
+    if len(commands) == 0:
+        pages = bot.formatter.format_help_for(ctx, bot)
+    elif len(commands) == 1:
+        name = _mention_pattern.sub(repl, commands[0])
+        command = None
+        if name in bot.cogs:
+            command = bot.cogs[name]
+        else:
+            command = bot.commands.get(name)
+            if command is None:
+                yield from bot.send_message(destination, bot.command_not_found.format(name))
+                return
+
+        pages = bot.formatter.format_help_for(ctx, command)
+    else:
+        name = _mention_pattern.sub(repl, commands[0])
+        command = bot.commands.get(name)
+        if command is None:
+            yield from bot.send_message(destination, bot.command_not_found.format(name))
+            return
 ''' 
     ----------------
         Commands
     ----------------	
 '''
 
-@moku.event
+
+@bot.event
 async def on_message(message):
 # prevent bot from replying to itself incase such a condition arises..
-    if message.author == moku.user:
-        await mokun_message_cleanup(message)
+    if message.author.bot:
+        # check if sent by self
+        if message.author.id == bot.user.id:
+            await botn_message_cleanup(message)
         return
-
-    else:
-        if message.content.startswith('~hello'):
-            msg = 'Hello {0.author.mention}'.format(message)
-            await moku.send_message(message.channel, msg)
-              
-            
-            
-        if message.content.startswith('~count'):
+        
+    if message.content.startswith(prefix): 
+        
+        fntMsg = message.content
+        fntMsg = fntMsg.replace(prefix, "")
+        
+       
+        
+        if fntMsg == 'count':
             counter = 0
-            tmp = await moku.send_message(message.channel, 'Calculating messages...')
-            async for log in moku.logs_from(message.channel, limit=100):
+            tmp = await bot.send_message(message.channel, 'Calculating messages...')
+            async for log in bot.logs_from(message.channel, limit=100):
                 if log.author == message.author:
                     counter += 1
-            await moku.edit_message(tmp, 'You have {} messages.'.format(counter))
-             
-            
-
-        if message.content.startswith('~sleep'):
+            await bot.edit_message(tmp, 'You have {} messages.'.format(counter))        
+        if fntMsg == 'sleep':
             await asyncio.sleep(15)
-            await moku.send_message(message.channel, 'Done sleeping')
+            await bot.send_message(message.channel, 'Done sleeping')
+        
         #delete all messages 
-        if message.content.startswith('~delete'):
+        if fntMsg == 'delete': #locking to allow only sudo permission users to do stuff
             mgs = []
-            tmp = await moku.send_message(message.channel, 'Clearing messages...')
+            tmp = await bot.send_message(message.channel, 'Clearing messages...')
             await asyncio.sleep(5)
-            async for mgs in moku.logs_from(message.channel, limit=100):
-                await moku.delete_message(mgs)
+            async for mgs in bot.logs_from(message.channel, limit=100):
+                await bot.delete_message(mgs)
+        if fntMsg == 'hello' :
+            msg = 'Hello {0.author.mention}'.format(message)
+            await bot.send_message(message.channel, msg)   
         
         
         
-            
+        ''' ---------------------------------
+                    REGION/ROLE ASSIGNMENT 
+            ---------------------------------
+        '''
+            #[will probably use this to help in matchmaking later on]
+            #can be swapped and used as role manager instead.
+        if message.content.startswith('~region'):
+            region = message.content[8:].upper()
+            role_exists = False
+            user_has_role = False
+            for role in message.server.roles:
+                if role.name == region:
+                    role_exists = True
+                    actual_role = role
+                    break
+            if not role_exists:
+                await bot.send_message(message.channel, ("Region does not exist.\nAvailable regions are {0}.".format(region_list)).strip('[]'))
+                return
+            if role_exists:
+                # Replace the region
+                for role in message.author.roles:  
+                    if role.name in region_list:  
+                        user_has_role = True
+                        try:
+                            await bot.remove_roles(message.author, role)
+                            await bot.add_roles(message.author, actual_role)
+                            await bot.send_message(message.channel, "Region changed to {0}.".format(region))
+                            return
+                        except discord.Forbidden:
+                            await bot.send_message(message.channel, "bot lacks the permissions to do that...")
+                            return    
+                if not user_has_role:  # user doesnt have the role yet
+                    try:
+                        await bot.add_roles(message.author, actual_role)
+                        await bot.send_message(message.channel, "Region set to {0}.".format(region))
+                    except discord.Forbidden:
+                        await bot.send_message(message.channel, "bot lacks the permissions to do that...")
+        elif message.content.startswith('~rmregion'):  #command to remove a role (need to tie this to mod only..)
+            for role in message.author.roles:  # Replace existing region
+                if role.name in region_list:  
+                    try:
+                        await bot.remove_roles(message.author, role)
+                        await bot.send_message(message.channel, "Role removed successfully.")
+                        return
+                    except discord.Forbidden:
+                        await bot.send_message(message.channel, "bot lacks the permissions to do that...")
+                        return    
+
+    await bot.process_commands(message)            
+      
+
+
 ''' 
     -------------------
         Auto Clean Up
     -------------------	
 '''
 
-#delete moku messages.        
-async def mokun_message_cleanup(message):
+#delete bot messages.        
+async def botn_message_cleanup(message):
     if message.channel.is_private:
         return
     if message.channel.permissions_for(message.server.me).manage_messages:
         await asyncio.sleep(60)
-        await moku.delete_message(message)
+        await bot.delete_message(message)
     else:
-        print("Moku lacks the permissions required to delete user messages.")
+        print("bot lacks the permissions required to delete user messages.")
 
 
 
-moku.run(token)
+bot.run(token)
